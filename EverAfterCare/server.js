28b2fa20359f23ce4d4638dae368e6bf148b7764
Alex Kao <alexkxosb@gmail.com>
2022-02-22 09:26:33 -0500

    Nettoyage et formattage du code dans server.js

--- EverAfterCare/server.js
+++ EverAfterCare/server.js
@@ -1,52 +1,34 @@
 /**
- * import all modules
- **/
-
+ * Importation des modules requis
+ */
 var express = require("express");
 var http = require("http");
 var mysql = require("mysql");
 var app = express();
 var bodyParser = require("body-parser");
 
-/*
-app.use((req, res, next) => {
-	console.log('Requête reçue !');
-	next(); // passer au prochain middleware
-  });
-  
-  app.use((req, res, next) => {
-	res.json({ message: 'Votre requête a bien été reçue !' });
-	next();
-  });
-  app.use((req, res, next) => {
-	console.log('Réponse envoyée avec succès !');
-	next();
-  });
-  */
-
-/*
- * parse all form data
+/**
+ * Parse all from data
  */
 app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = app;
-/*
- *used for formatting dates
+
+/**
+ * Pour le formattage des dates
  */
 var dateFormat = require("dateformat");
 const { debug } = require("console");
 var now = new Date();
 
-/*
- * view engine template parsing (ejs types)
+/**
+ * Pour activer EJS
  */
-
 app.set("view engine", "ejs");
 
 /**
- * import all related Javascript and css files to inject in our app
+ * Importation de tous les fichiers JS et CSS en lien pour l'application
  */
-
 app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
 app.use("/js", express.static(__dirname + "/node_modules/tether/dist/js"));
 app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
@@ -61,10 +43,10 @@ app.use(express.static("public"));
  * Pour que l'application puisse accéder aux images
  */
 app.use(express.static("images"));
-/**
- * connection à la BD
- */
 
+/**
+ * Connexion à la base de données "eac"
+ */
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
@@ -72,15 +54,21 @@ var con = mysql.createConnection({
 	database: "eac",
 });
 
-/*
- * Global site title and base url
+/**
+ * Connexion au serveur sur le port 4000
  */
+var server = app.listen(4000, function () {
+	console.log("serveur fonctionne sur 4000... ! ");
+});
 
+/**
+ * Constantes pour le titre et l'URL du site
+ */
 const siteTitle = "EverAfterCare";
 const baseURL = "http://localhost:4000/";
-/*
- * Envoyer le contenu au client
- * get the event list
+
+/**
+ * Pour generer la page d'accueil
  */
 app.get("/", function (req, res) {
 	con.query("SELECT * FROM client ORDER BY id DESC", function (err, result) {
@@ -92,50 +80,10 @@ app.get("/", function (req, res) {
 	});
 });
 
-app.get("/debug", function (req, res) {
-	con.query("SELECT * FROM client ORDER BY id DESC", function (err, result) {
-		res.render("pages/debug", {
-			siteTitle: siteTitle,
-			pageTitle: "Liste Client Debug",
-			items: result,
-		});
-	});
-});
-
-app.get("/debugD", function (req, res) {
-	con.query("SELECT * FROM docteur ORDER BY id DESC", function (err, result) {
-		res.render("pages/debug", {
-			siteTitle: siteTitle,
-			pageTitle: "Liste Docteur Debug",
-			items: result,
-		});
-	});
-});
-
-/* 
-fin de app.get(....)*/
-
-/*
-pour generer la page add event 
-*/
-
-app.get("/inscription", function (req, res) {
-	con.query("SELECT * FROM client ORDER BY id DESC", function (err, result) {
-		res.render("pages/inscription.ejs", {
-			siteTitle: siteTitle,
-			pageTitle: "Inscription",
-			items: result,
-		});
-	});
-});
-
-/*
-post method to data : pour ajouter un evenement à la BD
-*/
-
+/**
+ * Pour générer la page d'inscription
+ */
 app.post("/inscription", function (req, res) {
-	/* get the record base on ID
-	 */
 	var query =
 		"INSERT INTO client (id, first_name, last_name, email, password) VALUES (";
 	query += getRandomString(3) + ",";
@@ -150,6 +98,9 @@ app.post("/inscription", function (req, res) {
 	});
 });
 
+/**
+ * Pour générer la page de connexion
+ */
 app.get("/connexion", function (req, res) {
 	con.query("SELECT * FROM client ORDER BY id DESC", function (err, result) {
 		res.render("pages/connection.ejs", {
@@ -161,10 +112,9 @@ app.get("/connexion", function (req, res) {
 });
 
 /**
- * Pour la page de connexion des docteurs
+ * Pour générer la page de connexion des docteurs
  */
-
- app.get("/connexion/docteurs", function (req, res) {
+app.get("/connexion/docteurs", function (req, res) {
 	con.query("SELECT * FROM docteur ORDER BY id DESC", function (err, result) {
 		res.render("pages/connection.ejs", {
 			siteTitle: siteTitle,
@@ -174,13 +124,10 @@ app.get("/connexion", function (req, res) {
 	});
 });
 
-/*
-	post method to data : pour ajouter un evenement à la BD
-	*/
-
+/**
+ * Pour générer la page apres la connexion d'un client
+ */
 app.post("/connected", function (req, res) {
-	/* get the record base on ID
-	 */
 	var query = "SELECT * FROM client WHERE ";
 	query += "EMAIL = '" + req.body.email + "' AND ";
 	query += "PASSWORD = '" + req.body.password + "';";
@@ -211,12 +158,9 @@ app.post("/connected", function (req, res) {
 });
 
 /**
- * Pour la connexion des docteurs
+ * Pour générer la page apres la connexion des docteurs
  */
-
- app.post("/connectedDoctor", function (req, res) {
-	/* get the record base on ID
-	 */
+app.post("/connectedDoctor", function (req, res) {
 	var query = "SELECT * FROM docteur WHERE ";
 	query += "EMAIL = '" + req.body.email + "' AND ";
 	query += "PASSWORD = '" + req.body.password + "';";
@@ -246,10 +190,9 @@ app.post("/connected", function (req, res) {
 	});
 });
 
-/*
-pour editer un event 
-*/
-
+/**
+ * Pour modifier les rendez-vous
+ */
 app.get("/account/edit/:id", function (req, res) {
 	con.query(
 		"SELECT * FROM client WHERE id = '" + req.params.id + "'",
@@ -267,6 +210,24 @@ app.get("/account/edit/:id", function (req, res) {
 	);
 });
 
+/**
+ * Pour supprimer un rendez-vous
+ */
+app.get("/account/delete/:id", function (req, res) {
+	con.query(
+		"DELETE FROM client WHERE id = '" + req.params.id + "'",
+		function (err, result) {
+			if (err) throw err;
+			res.redirect(baseURL);
+		}
+	);
+});
+
+/**
+ * Pour générer un ID aléatoire
+ * @param {*} length
+ * @returns un nombre aléatoire
+ */
 function getRandomString(length) {
 	var randomChars = "0123456789";
 	var result = "";
@@ -278,13 +239,10 @@ function getRandomString(length) {
 	return result;
 }
 
-/*
-methode post a la bd : modifier un evenement
-*/
-
+/**
+ * Pour modifier les informations personnelles du client
+ */
 app.post("/account/edit/:id", function (req, res) {
-	/* get the record base on ID
-	 */
 	var query = "UPDATE  client SET";
 	query += " first_name = '" + req.body.first_name + "',";
 	query += " last_name = '" + req.body.last_name + "',";
@@ -297,23 +255,28 @@ app.post("/account/edit/:id", function (req, res) {
 	});
 });
 
-/*
-pour supprimer un event 
-*/
-
-app.get("/account/delete/:id", function (req, res) {
-	con.query(
-		"DELETE FROM client WHERE id = '" + req.params.id + "'",
-		function (err, result) {
-			if (err) throw err;
-			res.redirect(baseURL);
-		}
-	);
-});
 /**
- * connect to server
+ * Pour debug l'application - affichage des clients
  */
+app.get("/debug", function (req, res) {
+	con.query("SELECT * FROM client ORDER BY id DESC", function (err, result) {
+		res.render("pages/debug", {
+			siteTitle: siteTitle,
+			pageTitle: "Liste Client Debug",
+			items: result,
+		});
+	});
+});
 
-var server = app.listen(4000, function () {
-	console.log("serveur fonctionne sur 4000... ! ");
+/**
+ * Pour debug l'application - affichage des docteurs
+ */
+app.get("/debugD", function (req, res) {
+	con.query("SELECT * FROM docteur ORDER BY id DESC", function (err, result) {
+		res.render("pages/debug", {
+			siteTitle: siteTitle,
+			pageTitle: "Liste Docteur Debug",
+			items: result,
+		});
+	});
 });
