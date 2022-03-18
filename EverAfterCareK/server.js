6de5f5a32fcd947fce19d8f15d557184051f4c81
Alex Kao <alexkxosb@gmail.com>
2022-03-18 18:37:40 -0400

    Activation de EJS

--- EverAfterCareK/server.js
+++ EverAfterCareK/server.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true}));
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
