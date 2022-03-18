134cceedd363b63d8a5bde8527efaa63330a68fa
Alex Kao <alexkxosb@gmail.com>
2022-03-18 18:37:06 -0400

    Update des modules requis

--- EverAfterCareK/server.js
+++ EverAfterCareK/server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
-const user = require('./models/user');
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+const user = require("./models/user");
+const methodOverride = require("method-override");
+require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true}));
