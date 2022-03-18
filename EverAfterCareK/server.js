aa0f11059359034cbf5f1dd10b383e294f649aee
Alex Kao <alexkxosb@gmail.com>
2022-03-18 18:38:02 -0400

    Activer express-flash

--- EverAfterCareK/server.js
+++ EverAfterCareK/server.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: true}));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.flash());
 
 app.get('/', (req, res) => {
     res.render('index', {})
