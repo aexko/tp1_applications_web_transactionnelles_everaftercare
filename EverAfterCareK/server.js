f1eda519c59c4c24807f575171d4b710602b1b1b
Alex Kao <alexkxosb@gmail.com>
2022-03-18 17:32:13 -0400

    Activation de EJS

--- EverAfterCareK/server.js
+++ EverAfterCareK/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index', {})
 })
