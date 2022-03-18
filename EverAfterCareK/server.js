d55f608b8bf056cb988b008d2f0aee843addf744
Alex Kao <alexkxosb@gmail.com>
2022-03-18 17:33:11 -0400

    Pour parse les URL

--- EverAfterCareK/server.js
+++ EverAfterCareK/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true}));
 app.get('/', (req, res) => {
     res.render('index', {})
 })
