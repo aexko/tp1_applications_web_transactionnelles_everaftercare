a757c9423cfe82610cc79af918f599a903727a34
Adam7111 <2027111@bdeb.qc.ca>
2022-04-08 14:00:49 -0400

    Bismillah

--- EverAfterCareK/models/client.js
+++ EverAfterCareK/models/client.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     last_name: { type: String, required: true},
     email: { type: String, required: true},
     password: { type: String, required: true},
-    user_type: { type: String,"enum": ["client", "docteur"], default : "client", required: true}
+    user_type: { type: String,"enum": ["client", "docteur", "admin"], default : "client", required: true}
 });
 
 const user = mongoose.model('client', userSchema);
